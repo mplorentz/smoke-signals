0b85b614d7ff3dbf48604b461f64a076485d4d08
Matt Lorentz <matt@lorentz.is>
2014-07-08 15:43:09 -0400

    Turned off debug mode and fixed duplicate app running on Heroku

--- smokesignals/__init__.py
+++ smokesignals/__init__.py
@@ -1,5 +1,4 @@
 from flask import Flask
 app = Flask(__name__)
-debug = True
 
 import smokesignals.views
--- webserver.py
+++ webserver.py
@@ -1,2 +1,3 @@
 from smokesignals import app
-app.run(debug=True)
+if __name__ == "__main__":
+    app.run(debug=False)
